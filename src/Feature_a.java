ab489c888d227250d4f497e47e840f52c42d6081
Hasan SADEBIYIK <naturaturk11@gmail.com>
2022-12-12 23:26:20 +0300

    feature a be adviced

--- src/Feature_a.java
+++ src/Feature_a.java
@@ -2,5 +2,6 @@ public class Feature_a {
     public static void main(String[] args) {
         System.out.println("concret solutions were found");
         System.out.println("nonetheless it has bug");
+        System.out.println("need more practice");
     }
 }
