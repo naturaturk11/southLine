f39db2b682fc926952c9a142a02aff98863c9218
Hasan SADEBIYIK <naturaturk11@gmail.com>
2022-12-12 23:18:59 +0300

    doc1 has warning

--- src/Task1.java
+++ src/Task1.java
@@ -3,5 +3,6 @@
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("new option was added");
+        System.out.println("more features are neccesary should be added");
     }
 }
